d51f463ee502b23a10c3490373e26826de564adb
Dayane Garcia <daygds12@gmail.com>
2022-01-31 20:52:19 -0300

    atualizando h1 do login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components'
-import * as S from "./Login.styles"
+import * as S from "./Login.style"
 import Background from '../../img/background.jpg'
 import { logDOM } from "@testing-library/react";
-import Logo from '../../img/logo.jpg'
 
 
 
@@ -14,31 +13,26 @@ export default function Login() {
     return (
         
         <div>
-          <S.Navbar>Pokemon
-              
-           
-          </S.Navbar>
-          <S.Login>Acessar Pagina
-              <div>
-          <label for="Login">E-mail</label>
+            <S.Navbar>Pokemon</S.Navbar>
+            <S.Login>
+                <h3>Login</h3>
+        <div>
+            <label for="Login">E-mail</label>
             <input type="text" placeholder="Digite um email"/>
            
             <label for="Password">Senha</label>
             <input type="text" placeholder="Digite um email"/>
             <div>
-                <S.Button>Clique</S.Button>
-            </div>
-            </div>
-          </S.Login>
+             <S.Button>Clique</S.Button>
+        </div>
+         </div>
+            </S.Login>
 
-            <div>
-                <S.Imagem>
-            <img src={Background}/>
-            </S.Imagem>
-            </div>
+        
+            <S.Imagem src={Background}/>
            <div>
-           <S.Footer>@by Pokemon</S.Footer>
-           </div>
+            <S.Footer>@by Pokemon</S.Footer>
+        </div>
         </div>
     );
     }
--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -2,47 +2,74 @@ import styled from 'styled-components'
 
 
 export const Formulario = styled.form`
-color: red;
-
+    color: red;
     border-radius: 50%;
     position: absolute;
     top: -50%;
     left: 112px;  
     width: 100%;
     height: auto;  
-
+    
 `;
 
 export const Button = styled.button`
-   color: blue;
+   color: black;
+   
    border-radius: 8px;
 `
 
 export const Container = styled.body`
 
 background-size: cover;
-background: rgba(0, 0, 0, .4);
 opacity: 85%;
-
 
 `
 export const Login = styled.h1`
 border: 10px solid #000;
+font-size: 170%;
 width: 320px;
-height: 420px;
+height: 415px;
 border-radius:80px 0px 80px 0px;
-background: transparent;
 background-color: rgba(10,8,5,9,8);
 top:  40%;
-left: 45%;
+left: 30%;
 position: absolute;
 transform: translate(-555, -50%);
 box-sizing: border-box;
 padding: 35px 25px;
-color: black;
+color: #fff;
+background-color: black;
+opacity:78%;
+
+h3{
+   font-size: 95%;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
+`
 
 
+export const Nav = styled.nav`
+background-color: black;
+color: #fff;
+display: flex;
+padding: 1%;
+`
 
+export const Navbar = styled.header`
+ background-color: black; 
+ padding: 3%;
+ color: #fff;
+ display: flex;
+align-items: center;
+justify-content: center;
+
+`
+export const Imagem = styled.img`
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;    
 `
 
 export const Footer = styled.footer`
@@ -55,36 +82,4 @@ export const Footer = styled.footer`
  height: 100%;
  display: flex;
  position: relative;
-`
-
-export const Nav = styled.nav`
-background-color: black;
-color: #fff;
-display: flex;
-
-padding: 1%;
-`
-
-export const Navbar = styled.header`
- background-color: black; 
- padding: 3%;
- color: #fff;
- display: flex;
-align-items: center;
-justify-content: center;
-`
-
-export const Logo = styled.img`
-width: 15%;
-height: 15%;
-opacity: 52%;
-background-repeat: no-repeat;
-    
-
-`
-
-export const Imagem = styled.image`
-   background-repeat: no-repeat;
-  background-size: cover;
-        opacity: 72%;
 `
