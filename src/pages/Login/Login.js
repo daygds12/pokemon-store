e2cb5da82a7973c3666cdbde85ab13e646b5264d
Dayane Garcia <daygds12@gmail.com>
2022-01-31 18:06:18 -0300

    adicionando background

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components'
+import * as S from "./Login.styles"
+import Background from '../../img/background.jpg'
+import { logDOM } from "@testing-library/react";
+import Logo from '../../img/logo.jpg'
+
+
 
 export default function Login() {
 
     const navigate = useNavigate();
 
     return (
-        <div>Login</div>
-    
-    );
+        
+        <div>
+          <S.Navbar>Pokemon
+              
+           
+          </S.Navbar>
+          <S.Login>Acessar Pagina
+              <div>
+          <label for="Login">E-mail</label>
+            <input type="text" placeholder="Digite um email"/>
+           
+            <label for="Password">Senha</label>
+            <input type="text" placeholder="Digite um email"/>
+            <div>
+                <S.Button>Clique</S.Button>
+            </div>
+            </div>
+          </S.Login>
 
-}
+            <div>
+                <S.Imagem>
+            <img src={Background}/>
+            </S.Imagem>
+            </div>
+           <div>
+           <S.Footer>@by Pokemon</S.Footer>
+           </div>
+        </div>
+    );
+    }
---
+++ src/pages/Login/Login.style.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components'
+
+
+export const Formulario = styled.form`
+color: red;
+
+    border-radius: 50%;
+    position: absolute;
+    top: -50%;
+    left: 112px;  
+    width: 100%;
+    height: auto;  
+
+`;
+
+export const Button = styled.button`
+   color: blue;
+   border-radius: 8px;
+`
+
+export const Container = styled.body`
+
+background-size: cover;
+background: rgba(0, 0, 0, .4);
+opacity: 85%;
+
+
+`
+export const Login = styled.h1`
+border: 10px solid #000;
+width: 320px;
+height: 420px;
+border-radius:80px 0px 80px 0px;
+background: transparent;
+background-color: rgba(10,8,5,9,8);
+top:  40%;
+left: 45%;
+position: absolute;
+transform: translate(-555, -50%);
+box-sizing: border-box;
+padding: 35px 25px;
+color: black;
+
+
+
+`
+
+export const Footer = styled.footer`
+ background-color: black;
+ padding: 3%;
+ align-items: center;
+ justify-content:center;
+ color: #fff;
+ margin: 5%;
+ height: 100%;
+ display: flex;
+ position: relative;
+`
+
+export const Nav = styled.nav`
+background-color: black;
+color: #fff;
+display: flex;
+
+padding: 1%;
+`
+
+export const Navbar = styled.header`
+ background-color: black; 
+ padding: 3%;
+ color: #fff;
+ display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const Logo = styled.img`
+width: 15%;
+height: 15%;
+opacity: 52%;
+background-repeat: no-repeat;
+    
+
+`
+
+export const Imagem = styled.image`
+   background-repeat: no-repeat;
+  background-size: cover;
+        opacity: 72%;
+`
